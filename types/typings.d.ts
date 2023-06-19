0bbea89bbb9a740c6072d2d67fb7560efe990d68
Hunor-swag <mogyorosi.hunor@gmail.com>
2023-06-19 15:41:35 +0200

    deployment

---
+++ types/typings.d.ts
@@ -0,0 +1,20 @@
+export type Language = {
+  key: string;
+  name: string;
+};
+
+export type Guest = {
+  id: number;
+  name: string;
+  email: string;
+  address: string;
+  id_number: string;
+};
+
+export type User = {
+  id: number;
+  email: string;
+  name: string;
+  password: string;
+  phone_number: string;
+};
